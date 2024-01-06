11a0d8d301424d99918d339c0922e02e8b1f9911
jonathan <jonathan.123kutumbakana@gmail.com>
2024-01-06 14:43:39 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "John",
+    address: {
+      street: "123 Main St",}
+  };
+  function updateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    const newEmployee = {...employeeObject};
+    newEmployee[key] = value;
+    return newEmployee;
+  };
+  function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value) {
+    employeeObject[key] = value;
+    return employeeObject;
+  };
+  function deleteFromEmployeeByKey(employeeObject, key) {
+    const newEmployee = {...employeeObject};
+    delete newEmployee[key];
+    return newEmployee;
+  };
+  function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key];
+    return employeeObject;
+  };
